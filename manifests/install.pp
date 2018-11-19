3d9537aa3ad6133fc1a40b5bb8ae341f4e3d017d
root <root@ftcpjw1c.mylabserver.com>
2018-11-19 04:31:31 +0000

    fixed the package_name variable

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,7 +2,7 @@ class ntp::install (
   String $package_name   = $ntp::package_name,
   String $package_ensure = $ntp::package_ensure,
 ){
-  package { '$package_name':
+  package { "$package_name":
     ensure => $package_ensure,
 
   }
