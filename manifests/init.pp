6b9b5982b89b649030def1e1daa7e44d47ca485f
root <root@ftcpjw1c.mylabserver.com>
2018-11-19 04:21:44 +0000

    created the manifests and template files

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,7 +42,22 @@
 #
 # Copyright 2018 Your name here, unless otherwise noted.
 #
-class ntp {
+class ntp(
+  String $package_name        = $ntp::params::package_name,
+  String $package_ensure      = $ntp::params::package_ensure,
+  String $config_name         = $ntp::params::config_name,
+  String $config_file_mode    = $ntp::params::config_file_mode,
+  String $service_name        = $ntp::params::service_name,
+  Array[String] $servers      = $ntp::params::servers,
+  String $service_ensure      = $ntp::params::service_ensure,
+  Boolean $service_enable     = $ntp::params::service_enable,
+  Boolean $service_hasrestart = $ntp::params::service_hasrestart,
+  Boolean $service_hasstatus  = $ntp::params::service_hasstatus,
+) inherits ::ntp::params {
+  class { 'ntp::install': }
+  -> class { '::ntp::config':}
+  ~> class { '::ntp::service':}
+
 
 
 }
